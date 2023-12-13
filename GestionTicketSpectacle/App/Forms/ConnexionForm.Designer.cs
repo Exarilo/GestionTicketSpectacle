9f5cb0a5a1c11a10db60a0d36ccef9debc956093
Exarilo <romain.madery92@gmail.com>
2023-12-13 11:34:40 +0100

    IHM - update

--- GestionTicketSpectacle/App/Forms/ConnexionForm.Designer.cs
+++ GestionTicketSpectacle/App/Forms/ConnexionForm.Designer.cs
@@ -28,20 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.linkLbRegistration = new System.Windows.Forms.LinkLabel();
+            this.btValidate = new System.Windows.Forms.Button();
+            this.pbPasswordError = new System.Windows.Forms.PictureBox();
+            this.pbUsernameError = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPasswordError)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsernameError)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(22, 19);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(90, 13);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "Nom d\'utilisateur :";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(22, 88);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(77, 13);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Mot de passe :";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(25, 49);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(405, 20);
+            this.tbUsername.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(25, 116);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(405, 20);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // linkLbRegistration
+            // 
+            this.linkLbRegistration.AutoSize = true;
+            this.linkLbRegistration.Location = new System.Drawing.Point(175, 153);
+            this.linkLbRegistration.Name = "linkLbRegistration";
+            this.linkLbRegistration.Size = new System.Drawing.Size(49, 13);
+            this.linkLbRegistration.TabIndex = 4;
+            this.linkLbRegistration.TabStop = true;
+            this.linkLbRegistration.Text = "S\'inscrire";
+            this.linkLbRegistration.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLbRegistration_LinkClicked);
+            // 
+            // btValidate
+            // 
+            this.btValidate.Location = new System.Drawing.Point(165, 178);
+            this.btValidate.Name = "btValidate";
+            this.btValidate.Size = new System.Drawing.Size(75, 23);
+            this.btValidate.TabIndex = 16;
+            this.btValidate.Text = "Valider";
+            this.btValidate.UseVisualStyleBackColor = true;
+            this.btValidate.Click += new System.EventHandler(this.btValidate_Click);
+            // 
+            // pbPasswordError
+            // 
+            this.pbPasswordError.BackgroundImage = global::GestionTicketSpectacle.Properties.Resources.error;
+            this.pbPasswordError.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbPasswordError.Location = new System.Drawing.Point(436, 116);
+            this.pbPasswordError.Name = "pbPasswordError";
+            this.pbPasswordError.Size = new System.Drawing.Size(22, 20);
+            this.pbPasswordError.TabIndex = 15;
+            this.pbPasswordError.TabStop = false;
+            this.pbPasswordError.Visible = false;
+            // 
+            // pbUsernameError
+            // 
+            this.pbUsernameError.BackgroundImage = global::GestionTicketSpectacle.Properties.Resources.error;
+            this.pbUsernameError.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbUsernameError.Location = new System.Drawing.Point(436, 49);
+            this.pbUsernameError.Name = "pbUsernameError";
+            this.pbUsernameError.Size = new System.Drawing.Size(22, 20);
+            this.pbUsernameError.TabIndex = 14;
+            this.pbUsernameError.TabStop = false;
+            this.pbUsernameError.Visible = false;
+            // 
+            // ConnexionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(469, 221);
+            this.Controls.Add(this.btValidate);
+            this.Controls.Add(this.pbPasswordError);
+            this.Controls.Add(this.pbUsernameError);
+            this.Controls.Add(this.linkLbRegistration);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ConnexionForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPasswordError)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsernameError)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.LinkLabel linkLbRegistration;
+        private System.Windows.Forms.PictureBox pbUsernameError;
+        private System.Windows.Forms.PictureBox pbPasswordError;
+        private System.Windows.Forms.Button btValidate;
     }
 }
 
--- GestionTicketSpectacle/App/Forms/ConnexionForm.cs
+++ GestionTicketSpectacle/App/Forms/ConnexionForm.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using GestionTicketSpectacle.App.Forms;
+using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +7,55 @@ namespace GestionTicketSpectacle
 {
     public partial class ConnexionForm : Form
     {
+        private InscriptionForm inscription;
         public ConnexionForm()
         {
             InitializeComponent();
+        }
+
+        private void linkLbRegistration_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (inscription == null || inscription.IsDisposed)
+            {
+                inscription = new InscriptionForm();
+                inscription.FormClosed += InscriptionFormClosed; 
+            }
+
+            this.Hide(); 
+            inscription.Show();
+        }
+        private void InscriptionFormClosed(object sender, EventArgs e)
+        {
+            this.Show(); 
+        }
+
+        private async void btValidate_Click(object sender, EventArgs e)
+        {
+            await HandleEmptyField(tbUsername.Text, pbUsernameError);
+            await HandleEmptyField(tbPassword.Text, pbPasswordError);
+        }
+
+        private async Task HandleEmptyField(string fieldText, PictureBox pictureBox)
+        {
+            if (string.IsNullOrWhiteSpace(fieldText))
+            {
+                await BlinkIcon(pictureBox);
+            }
+            else
+            {
+                pictureBox.Visible = false;
+            }
+        }
+
+        private async Task BlinkIcon(PictureBox pictureBox)
+        {
+            int numberOfBlinks = 6; 
+            for (int i = 0; i < numberOfBlinks; i++)
+            {
+                pictureBox.Visible = !pictureBox.Visible;
+                await Task.Delay(150); 
+            }
+            pictureBox.Visible = true; 
         }
     }
 }
